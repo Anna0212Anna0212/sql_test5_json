6ee26829a62ac19f65ae90f86b1a585b9b0bb872
揚恩 張 <anna0983266723@gmail.com>
2025-01-21 15:39:11 +0800

    加入專案檔案。

---
+++ sql_test5/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace sql_test5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.about = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.traffic = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.name,
+            this.about,
+            this.address,
+            this.traffic,
+            this.time});
+            this.dataGridView1.Location = new System.Drawing.Point(26, 95);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(744, 329);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // name
+            // 
+            this.name.DataPropertyName = "name";
+            this.name.HeaderText = "name";
+            this.name.Name = "name";
+            // 
+            // about
+            // 
+            this.about.DataPropertyName = "about";
+            this.about.HeaderText = "about";
+            this.about.Name = "about";
+            // 
+            // address
+            // 
+            this.address.DataPropertyName = "address";
+            this.address.HeaderText = "address";
+            this.address.Name = "address";
+            // 
+            // traffic
+            // 
+            this.traffic.DataPropertyName = "traffic";
+            this.traffic.HeaderText = "traffic";
+            this.traffic.Name = "traffic";
+            // 
+            // time
+            // 
+            this.time.DataPropertyName = "time";
+            this.time.HeaderText = "time";
+            this.time.Name = "time";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn about;
+        private System.Windows.Forms.DataGridViewTextBoxColumn address;
+        private System.Windows.Forms.DataGridViewTextBoxColumn traffic;
+        private System.Windows.Forms.DataGridViewTextBoxColumn time;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ sql_test5/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.Json;
+using System.IO;
+
+namespace sql_test5
+{
+    public partial class Form1 : Form
+    {
+        Database1Entities db = new Database1Entities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+                string JsonString = File.ReadAllText("COD03_景點清單附檔模擬.json");
+                List<test5> JsonData = JsonSerializer.Deserialize<List<test5>>(JsonString);
+
+                int ID = 0;
+                foreach (test5 Table in JsonData)
+                {
+                    Table.Id = ID++;
+                    db.test5.Add(Table);
+                }
+                db.SaveChanges();
+
+                DataTable dt = new DataTable();
+                dt.Columns.Add("name");
+                dt.Columns.Add("about");
+                dt.Columns.Add("address");
+                dt.Columns.Add("traffic");
+                dt.Columns.Add("time");
+
+                foreach (var table in db.test5)
+                {
+                    dt.Rows.Add(table.name, table.about, table.address, table.traffic, table.time);
+                }
+                // 將 DataTable 設定為 DataGridView 的資料來源
+                dataGridView1.DataSource = dt;
+            
+        }
+    }
+}
